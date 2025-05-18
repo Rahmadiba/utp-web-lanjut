7f546237673fe1cad7f68374366a4a2b1b11452a
Rahmadiba <afifahsamudra9@gmail.com>
2025-05-19 03:40:11 +0700

    Part 11

--- database/migrations/2025_05_18_202537_create_payments_table.php
+++ database/migrations/2025_05_18_202537_create_payments_table.php
@@ -11,8 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('enrollments', function (Blueprint $table) {
+        Schema::create('payments', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('enrollment_id');
+            $table->date('paid_date');
+            $table->double('amount');
             $table->timestamps();
         });
     }
@@ -22,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('enrollments');
+        Schema::dropIfExists('payments');
     }
 };
